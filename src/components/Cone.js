ba3b5dbbcd440d1a0a982e0d7895001fc4ccfd0c
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-27 01:00:04 +0530

    🩹Key to cone stripes

--- src/components/Cone.js
+++ src/components/Cone.js
@@ -12,13 +12,13 @@ export default function Cone() {
         }}
       />
       {[0, 0, 0].map((_, index) => (
-        <line x1={38 * (index + 1)} y1="3" x2="75" y2="200" style={{
+        <line key={index} x1={38 * (index + 1)} y1="3" x2="75" y2="200" style={{
           stroke: "tan",
           strokeWidth: 4,
         }} />
       ))}
       {[0, 0, 0, 0].map((_, index) => (
-        <line x1={16 + index * 16} y1={38 * (index + 1)} x2={150 - 16 - index * 16} y2={38 * (index + 1)} style={{
+        <line key={index} x1={16 + index * 16} y1={38 * (index + 1)} x2={150 - 16 - index * 16} y2={38 * (index + 1)} style={{
           stroke: "tan",
           strokeWidth: 4,
         }} />
