cf5ddeac74f235c1967e3dd9360767204164716c
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-27 00:56:51 +0530

    🍱 Icons and Icon exports

---
+++ src/constants/TOPPINGS.js
@@ -0,0 +1,11 @@
+import cherry from '../assets/cherry.svg';
+import wafer from '../assets/wafer-icon.svg'
+import sprinkles from '../assets/sprinkles.svg';
+
+const TOPPINGS = {
+  Cherry: cherry,
+  Wafer: wafer,
+  Sprinkles: sprinkles,
+};
+
+export default TOPPINGS;
