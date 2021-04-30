bded3e5205cdca89325d21bd4000364a9b123fb0
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-30 23:43:13 +0530

    WIP: Make Your Own

--- src/components/MakeYourOwnFAB.js
+++ src/components/MakeYourOwnFAB.js
@@ -3,10 +3,14 @@ import scoopIcon from '../assets/scoop.svg';
 
 const MakeYourOwnFAB = () => {
   return (
-    <Link to="/ice-cream-editor">
-      <button className="bg-pink-600 sm:px-3.5 transform transition-all hover:scale-110 bg-transparent rounded-full fixed bottom-4 right-4 hover:shadow sm:bottom-8 sm:right-8">
+    <Link to="/ice-cream-editor" >
+      <button style={{
+        cursor: `url(/barricade.png), auto`,
+      }} className="bg-pink-600 sm:px-3.5 transform transition-all hover:scale-110 bg-transparent rounded-full fixed bottom-4 right-4 hover:shadow sm:bottom-8 sm:right-8">
         <img className="block sm:hidden" width={56} height={56} src={scoopIcon} alt="Scoop" />
-        <p className="hidden leading-9 sm:block text-center text-white text-sm font-semibold uppercase">
+        <p style={{
+          cursor: `url(/barricade.png), auto`,
+        }} className="hidden leading-9 sm:block text-center text-white text-sm font-semibold uppercase">
           Make your Own
         </p>
       </button>
